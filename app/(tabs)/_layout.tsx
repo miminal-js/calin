c7d1d50cc0f262b329a905d0dab32372d3207948
Temilola Adepoju <temiadepoju384@gmail.com>
2026-07-30 15:19:14 -0500

    changed icons of navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,11 +1,15 @@
+import { Image } from 'expo-image';
 import { Tabs } from 'expo-router';
+const dashpic = require('@/assets/images/callie-dashboard.png');
+const nutrition = require('@/assets/images/callie-hungry.png');
+const community = require('@/assets/images/callie-community.png');
 
 export default function TabLayout() {
   return (
     <Tabs>
-      <Tabs.Screen name="dashboard" options={{ title: 'Home' }} />
-      <Tabs.Screen name="foodtrack" options={{ title: 'Nourishment' }} />
-      <Tabs.Screen name="community" options={{ title: 'Community' }} />
+      <Tabs.Screen name="dashboard" options={{ title: 'Home', tabBarIcon: () => <Image source={dashpic} style={{ width: 48, height: 48 }} /> }} />
+      <Tabs.Screen name="foodtrack" options={{ title: 'Nourishment', tabBarIcon: () => <Image source={nutrition} style={{ width: 48, height: 48 }} /> }} />
+      <Tabs.Screen name="community" options={{ title: 'Community', tabBarIcon: () => <Image source={community} style={{ width: 48, height: 48 }} /> }} />
     </Tabs>
   );
 }
